41b24ae43ec728c6074b659ee34c676f8094a49a
Fasith-23 <ed20b019@smail.iitm.ac.in>
2024-08-13 01:45:11 +0530

    Update next.config.mjs - remove base path

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    basePath: "/rs_vision_documentation",
     output: "export",  // <=== enables static exports
+    basePath: process.env.NODE_ENV === "production" ? "": undefined,
     reactStrictMode: true,
     images: { unoptimized: true }
 };
